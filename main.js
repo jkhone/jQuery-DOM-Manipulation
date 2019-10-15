1b44163b8dcba6b8e0627a7d473dd9700a17d42c
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-10-15 14:24:27 -0700

    Done

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-$(document).ready(function($) {
-    var allSections = $('.content > p').hide()
+// $(document).ready(function() {
+//     var allSections = $('.content > p').hide()
 
-    $('.content > h2 > a').click(function() {
-        allSections.slideUp()
-        $(this).parent().next().slideDown()
-        return false
+//     $('.content > h2 > a').click(function() {
+//         allSections.slideUp()
+//         $(this).parent().next().slideDown()
+//         return false
+//     })
+
+
+// })
+
+$(document).ready(function() {
+    $(".section").on('click', 'a', function(e) {
+        var tab = $(this).attr('href')
+        $('.tab').removeClass('active')
+        $(tab).addClass('active')
     })
-
-
-})($)
+})
